0e067f2076cf1e9c75a7184d08d8b600661020e8
Kabelin <37315628+Kabelin@users.noreply.github.com>
2018-05-23 20:36:39 -0300

    Add files via upload

---
+++ P6.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int soma(int*,int*);
+float div(float,float);
+
+int main(int argc, char ** argv) {
+	//Vitor Rabelo Cruvinel - 11721ECP004.
+	int n[20],q,i=0;
+	printf("Qual a quantidade de numeros?\n");
+	scanf("%d\n",&q);getchar();
+	while(i<20){
+		n[i]=0;
+		i++;
+	}
+	i=0;
+	while(i<q){
+		printf("Numero %d\n: ",i+1);
+		scanf("%d",&n[i]);getchar();
+		i++;
+	}
+	printf("Media = %f",div(q,(soma(&n[0],&n[q-1]))));
+	return 0;
+}
+
+int soma(int *ref,int *px){
+	return (*px) + ((ref == px) ? 0 : soma(ref, px-1));
+}
+float div(float q, float n){
+	return n/q;
+}
---
+++ P7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int A(int,int);
+
+int main(int argc, char ** argv) {
+	//Vitor Rabelo Cruvinel - 11721ECP004.
+	int m,n;
+	printf("Digite m e n:\n");
+	scanf("%d%d",&m,&n);
+	printf("\nResultado: %d",A(m,n));
+	return 0;
+}
+
+int A(int m,int n){
+	if(m==0) return n+1;
+	else if((m>0) && (n==0)) return A(m-1,1);
+	else if((m>0) && (n>0)) return A(m-1,A(m,n-1));
+	return 0;
+}
