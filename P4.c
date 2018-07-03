6ac268f296d434684b1ffbce83132f595ead369e
Kabelin <37315628+Kabelin@users.noreply.github.com>
2018-07-02 21:13:16 -0300

    Update P4.c

--- P4.c
+++ P4.c
@@ -2,15 +2,17 @@
 #include <stdio.h>
 int main(int argc, char ** argv) {
 	char num[256];
-	int i=0,aux;
+	int i=0,aux,n=0;
 	printf("Digite a string\n");
 	scanf("%s", num);
 	while(num[i]!='\0'){
 		aux=48;
 		while(aux>=48 && aux<=57){
-			if(num[i]==aux) printf("%c",num[i]);
+			if(num[i]==aux) {printf("%c",num[i]);n++;}
 			aux++;
 		}
 		i++;
 	}
+	if(n==0) printf("0");
+	return 0;
 }
